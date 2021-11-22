60eaa57929729c262fa278650885fc3587cb163b
YeFFreY <johan.mslms@outlook.com>
2021-11-22 17:34:37 +0100

    User can create a course in a calendar with [sc-83]

--- src/main/java/org/yeffrey/cheesecake/features/course/create/CreateCourseCommand.java
+++ src/main/java/org/yeffrey/cheesecake/features/course/create/CreateCourseCommand.java
@@ -3,10 +3,10 @@ package org.yeffrey.cheesecake.features.course.create;
 import io.micronaut.core.annotation.Introspected;
 
 import javax.validation.constraints.NotNull;
-import java.time.ZonedDateTime;
+import java.time.OffsetDateTime;
 import java.util.UUID;
 
 @Introspected
-public record CreateCourseCommand(@NotNull UUID calendarId, @NotNull UUID classId, @NotNull ZonedDateTime start,
-                                  @NotNull ZonedDateTime end) {
+public record CreateCourseCommand(@NotNull UUID calendarId, @NotNull UUID classId, @NotNull OffsetDateTime start,
+                                  @NotNull OffsetDateTime end) {
 }
--- src/test/groovy/org/yeffrey/cheesecake/CheesecakeSpecification.groovy
+++ src/test/groovy/org/yeffrey/cheesecake/CheesecakeSpecification.groovy
@@ -3,8 +3,11 @@ package org.yeffrey.cheesecake
 import com.github.javafaker.Faker
 import io.micronaut.context.ApplicationContext
 import jakarta.inject.Inject
+import org.yeffrey.cheesecake.core.Constants
 import spock.lang.Shared
 import spock.lang.Specification
+
+import java.time.format.DateTimeFormatter
 
 class CheesecakeSpecification extends Specification {
     @Inject
@@ -12,6 +15,8 @@ class CheesecakeSpecification extends Specification {
 
     @Shared
     Faker faker = new Faker()
+
+    static DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern(Constants.DATE_TIME_FORMAT)
 
     UUID getBobUUID() {
         return UUID.fromString("8d9d461d-e78a-4c9d-a6ba-428a481b044d")
